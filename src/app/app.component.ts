af3b5a86fff337145b6f3157b4221d57f1a54c80
AnuBapatla <anusrichatala@gmail.com>
2026-02-17 19:52:52 -0500

    interpolation worked

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TestComponent } from './test/test.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,TestComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'my-angular-app1';
+  title = 'welcome';
+  msg ="Hello from AppComponent!"
 }
