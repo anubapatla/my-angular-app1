dd8027dd44f82aca52bbf0744dc6c33e04e453e1
AnuBapatla <anusrichatala@gmail.com>
2026-02-22 22:57:08 -0500

    static way binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TestComponent } from './test/test.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,TestComponent],
+  imports: [RouterOutlet,TestComponent,FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  onKeyUp(){
-    console.log("keyup event triggered");
+  staticInput :string ="helloworld";
   }
   
   
-}
+
