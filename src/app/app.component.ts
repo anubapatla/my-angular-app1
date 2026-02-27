af4bd49883bcc907d189991f405f0b9d160447e0
AnuBapatla <anusrichatala@gmail.com>
2026-02-26 22:42:11 -0500

    structral directive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,19 +2,17 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TestComponent } from './test/test.component';
 import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,TestComponent,FormsModule],
+  imports: [RouterOutlet, TestComponent,CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  quantity :number =1;
-  pricePerItem:number =100;
-  get totalPrice(): number{
-    return this.quantity*this.pricePerItem
-  }
+  display: boolean = true;
   }
   
   
