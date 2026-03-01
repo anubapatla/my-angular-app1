e79c41c38428d4c2856bf6cc3e69c27f4314c7a4
AnuBapatla <anusrichatala@gmail.com>
2026-02-28 22:21:29 -0500

    controlflow statements

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TestComponent } from './test/test.component';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 
+interface empInterface {
+  id: number;
+  name: string;
+  role: string;
+}
 
 @Component({
   selector: 'app-root',
@@ -12,9 +17,17 @@ import { CommonModule } from '@angular/common';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  displayElement : boolean = false;
-  show(){
-    this.displayElement=!this.displayElement;
+  showEmployees: boolean =true;
+  employees = signal<empInterface[]>([
+    {id:1,name:'EmploeeA',role:'Admin'},
+    {id:2,name:'EmploeeB',role:'Manager'},
+    {id:3,name:'EmploeeC',role:'Developer'},
+    {id:4,name:'EmploeeD',role:'Tester'},
+    {id:5,name:'EmploeeE',role:'Designer'}
+  ]);
+  toggleEmployee(){
+    this.showEmployees = !this.showEmployees;
+
   }
 }
   
