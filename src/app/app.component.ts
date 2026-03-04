5c339d66d880152f2796f19897e48d80928cb01d
AnuBapatla <anusrichatala@gmail.com>
2026-03-04 14:57:09 -0500

    control flow atatement with @switch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,12 +12,25 @@ interface empInterface {
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, TestComponent,CommonModule],
+  imports: [RouterOutlet, TestComponent, CommonModule, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  person: any = { age: 90 };
+employees() {
+throw new Error('Method not implemented.');
+}
+  employeeRole:string='';
+  employeeList = signal<empInterface[]>([
+    { id:1,name:'EmployeeA',role:'Developer'},
+    { id:2,name:'EmployeeB',role:'Marketing'},
+    { id:3,name:'EmployeeC',role:'Manager'},
+    { id:4,name:'EmployeeD',role:'Hr'},
+    { id:5,name:'EmployeeE',role:'Analyst'},
+    { id:6,name:'EmployeeF',role:'Admin'},
+
+    
+  ])
 }
   
   
