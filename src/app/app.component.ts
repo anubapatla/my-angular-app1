eb5877fc134e293ec6b63776baf187ed81328f79
AnuBapatla <anusrichatala@gmail.com>
2026-03-05 18:22:04 -0500

    AGE VALIDATION USING NGSWITCH

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,33 +4,21 @@ import { TestComponent } from './test/test.component';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 
-interface empInterface {
-  id: number;
-  name: string;
-  role: string;
-}
 
 @Component({
   selector: 'app-root',
+  standalone: true,
   imports: [RouterOutlet, TestComponent, CommonModule, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-employees() {
-throw new Error('Method not implemented.');
-}
-  employeeRole:string='';
-  employeeList = signal<empInterface[]>([
-    { id:1,name:'EmployeeA',role:'Developer'},
-    { id:2,name:'EmployeeB',role:'Marketing'},
-    { id:3,name:'EmployeeC',role:'Manager'},
-    { id:4,name:'EmployeeD',role:'Hr'},
-    { id:5,name:'EmployeeE',role:'Analyst'},
-    { id:6,name:'EmployeeF',role:'Admin'},
+age:number =0;
+isEligible:boolean =false;
 
-    
-  ])
+checkEligibility(){
+  this.isEligible=this.age >=18;
+}
 }
   
   
