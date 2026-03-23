a72aa0cc0a9f924dc4e0b9cd6d0a31f4287529a8
AnuBapatla <anusrichatala@gmail.com>
2026-03-23 13:55:02 -0400

    Aftercontent checked

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -1,4 +1,4 @@
-import { AfterContentInit, Component,ContentChild,DoCheck,ElementRef,Input,OnChanges, SimpleChanges, ViewChild, viewChild } from '@angular/core';
+import { AfterContentChecked, AfterContentInit, Component,ContentChild,DoCheck,ElementRef,Input,OnChanges, SimpleChanges, ViewChild, viewChild } from '@angular/core';
 import { JsonPipe } from '@angular/common';
 
 @Component({
@@ -7,12 +7,15 @@ import { JsonPipe } from '@angular/common';
   templateUrl: './test.component.html',
   styleUrl: './test.component.css'
 })
-export class TestComponent  implements AfterContentInit{
+export class TestComponent  implements AfterContentInit , AfterContentChecked{
   @ViewChild('wrapper')wrapper!:ElementRef;
   @ContentChild('contentWrapper')content!:ElementRef;
   ngAfterContentInit(): void {
     console.log('ngAfterContentInit() hook was invoked..');
      console.log('ngAfterContentInit()-wrapper:',this.wrapper);
       console.log('ngAfterContentInit() - content:' ,this.content);
+  }
+  ngAfterContentChecked(): void {
+    console.log('ngAfterContentChecked() hook is invoked....')
   }
 }
