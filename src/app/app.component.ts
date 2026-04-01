d4e680131fa719f1975b377ee6cea5f29fff53e8
AnuBapatla <anusrichatala@gmail.com>
2026-04-01 13:12:24 -0400

    binding properties with hostbind

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,13 @@ import { RouterOutlet } from '@angular/router';
 import { TestComponent } from './test/test.component';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { HighlightElementDirective } from "./highlight-element.directive";
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, TestComponent, CommonModule, FormsModule],
+  imports: [RouterOutlet, HighlightElementDirective, CommonModule, FormsModule, HighlightElementDirective],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/highlight-element.directive.ts
+++ src/app/highlight-element.directive.ts
@@ -1,10 +1,13 @@
-import { Directive } from '@angular/core';
+import { Directive, HostBinding } from '@angular/core';
 
 @Directive({
   selector: '[appHighlightElement]'
 })
 export class HighlightElementDirective {
-
-  constructor() { }
+ @HostBinding('style.backgroundColor') bgcolor = 'yellow';
+ @HostBinding('style.padding') padding = '10px';
+ @HostBinding('style.border') border = '2px solid black';
+ @HostBinding('style.color') color = 'red';
+ constructor() { }
 
 }
