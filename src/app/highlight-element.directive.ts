13eae4f96ff7ce6331fd94a8d5b86ec3dcf3d6d5
AnuBapatla <anusrichatala@gmail.com>
2026-04-02 16:24:06 -0400

    method decorator host listner()  have been added

--- src/app/highlight-element.directive.ts
+++ src/app/highlight-element.directive.ts
@@ -1,13 +1,31 @@
-import { Directive, HostBinding } from '@angular/core';
+import { Directive, HostBinding, HostListener, OnInit } from '@angular/core';
 
 @Directive({
   selector: '[appHighlightElement]'
 })
-export class HighlightElementDirective {
+export class HighlightElementDirective implements OnInit{
  @HostBinding('style.backgroundColor') bgcolor = 'yellow';
  @HostBinding('style.padding') padding = '10px';
  @HostBinding('style.border') border = '2px solid black';
  @HostBinding('style.color') color = 'red';
- constructor() { }
+ colorArray = ['red','blue','green','orange','purple'];
+  private setRandomColor(){
+  const colorPick = Math.floor(Math.random() * this.colorArray.length);
+  //this.color = this.colorArray[randomIndex];
+  if(this.bgcolor !== this.colorArray[colorPick]){
+    this.bgcolor = this.colorArray[colorPick];
+    
+  }else{
+    this.setRandomColor();
+  }
+}
+  
+ @HostListener('click') onToggle(){
+  this.setRandomColor();}
+ //constructor() { }
+  ngOnInit(): void {
+    this.bgcolor = 'lightblue';
+    this.color = 'darkblue';
+  }
 
 }
