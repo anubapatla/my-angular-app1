980028978ffeb1b34d8cd142b084b99ae51964e4
AnuBapatla <anusrichatala@gmail.com>
2026-04-11 17:33:50 -0400

    cutsom pipe appendtext has been created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,26 +4,18 @@ import { TestComponent } from './test/test.component';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { HighlightElementDirective } from "./highlight-element.directive";
+import { AppendTextPipe } from './append-text.pipe';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule, FormsModule, HighlightElementDirective],
+  imports: [RouterOutlet, CommonModule, FormsModule, AppendTextPipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  showJson:boolean=false;
-students =[
-  {name:'userA',age:20,gender:'male',major:'css'},
-   {name:'user',age:22,gender:'female',major:'angular'},
-    {name:'userC',age:25,gender:'female',major:'javascript'},
-     {name:'userD',age:24,gender:'male',major:'c#'},
-];
-toggleJson(){
- this.showJson=!this.showJson;
-}
+ 
 title(title: any) {
     throw new Error('Method not implemented.');
   }
