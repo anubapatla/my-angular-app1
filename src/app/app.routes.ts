209d5e3a8898e8114a9eb7b870469808306e0c8f
AnuBapatla <anusrichatala@gmail.com>
2026-04-14 22:41:32 -0400

    creating navbar using routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,16 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { WorkComponent } from './work/work.component';
+import { NewsComponent } from './news/news.component';
+import { ServicesComponent } from './services/services.component';
 
 export const routes: Routes = [
-    {path:'home', component: HomeComponent}
+    {path:'home', component: HomeComponent},
+    {path:'about', component: AboutComponent},
+    {path:'contact', component: ContactComponent},
+    {path:'work', component: WorkComponent},
+    {path:'news', component: NewsComponent},
+    {path:'services', component: ServicesComponent}
 ];
