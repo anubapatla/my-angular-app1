cc08cc66307333681d6d8d231f653f5a7261d063
AnuBapatla <anusrichatala@gmail.com>
2026-04-25 23:26:55 -0400

    nopreloading

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,11 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { PreloadAllModules, provideRouter, withPreloading } from '@angular/router';
+import { NoPreloading, PreloadAllModules, provideRouter, withPreloading } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes,withPreloading(PreloadAllModules)), provideClientHydration(withEventReplay())]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }),
+     provideRouter(routes,withPreloading(NoPreloading)), 
+     provideClientHydration(withEventReplay())]
 };
